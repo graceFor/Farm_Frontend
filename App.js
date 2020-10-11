d64cae155664642a4438fe01ae9676bc0ef85921
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-11 19:13:58 +0900

    App.js => Complete loadAssets function
    cacheImages, cacheFonts의 반환값은 Promise

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
-import { Text, Image } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 
-const cacheImages = (images) =>
-  images.map((image) => {
+const cacheImages = images =>
+  images.map(image => {
     if (typeof image === "string") {
       return Image.prefetch(image);
     } else {
@@ -14,21 +14,29 @@ const cacheImages = (images) =>
     }
   });
 
-const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const cacheFonts = fonts =>
+  fonts.map(font => [Font.loadAsync(font), Font.loadAsync(font)]);
 
-export default function App() {
+export default function App() { // App 로드 될 때 미리 로드 
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = cacheImages([
-      "https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      require("./assets/splash.png"),
+      "https://images.unsplash.com/photo-1562887189-e5d078343de4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      require("./assets/splash.png")
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
   return isReady ? (
-    <Text>Ready!</Text>
+    <View>
+      <Text>Ready!</Text>
+    </View>
   ) : (
-    <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
