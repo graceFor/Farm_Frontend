d2b3313254fa805be0a537f934f1fad4929b4ca5
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-15 19:43:44 +0900

    [New Screen to Shop]

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from "@react-navigation/stack";
 import Home from '../screens/Home';
 import Place from '../screens/Place';
 import Recipe from '../screens/Recipe';
+import Shop from '../screens/Shop';
 
 const Stack = createStackNavigator();
 
@@ -11,5 +12,6 @@ export default ( )=> {
         <Stack.Screen name ="Home" component={Home} />
         <Stack.Screen name = "Place" component={Place} />
         <Stack.Screen name = "Recipe" component={Recipe} />
+        <Stack.Screen name = "Shop" component={Shop} />
     </Stack.Navigator>
 }
