9f97e022564d730794203e479a2bbb217fceb13e
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-10-23 22:47:08 +0900

    Together

--- screens/Home.js
+++ screens/Home.js
@@ -7,4 +7,5 @@ export default ({navigation}) => (
     <Button onPress={()=>navigation.navigate("Place")} title="Go to Detail" />
     <Button onPress={()=>navigation.navigate("Recipe")} title="Go to Detail" />
     <Button onPress={()=>navigation.navigate("Shop")} title="Go to Detail" />
+    <Button onPress={()=>navigation.navigate("Together")} title="Go to Detail" />
 </View>)
