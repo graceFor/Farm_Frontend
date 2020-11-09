0efabf2f2442cbc45a5230b8415b61623002f176
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2020-11-09 23:08:06 +0900

    Navigation

--- screens/Home.js
+++ screens/Home.js
@@ -5,9 +5,9 @@ export default ({ navigation }) => {
   return (
     <View>
       <Text>Home</Text>
-      <Button onPress={() => navigation.navigate("Place")} title="Go to Place" />
+      <Button onPress={() => navigation.navigate("Place")} title="어디에 있을까?" />
       <Button onPress={() => navigation.navigate("Recipe")} title="Go to Recipe" />
-      <Button onPress={() => navigation.navigate("Shop")} title="Go to Shop" />
+      <Button onPress={() => navigation.navigate("Shop")} title="Shop" />
       <Button onPress={() => navigation.navigate("Together")} title="Go to Together" />
       <Button onPress={() => navigation.navigate("Help")} title="Go to Help" />
     </View>
